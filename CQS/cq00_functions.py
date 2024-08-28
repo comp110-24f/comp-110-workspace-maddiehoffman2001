761d720590bb8ed74b13969697acfce94bdc177a
MEHOFF@UNC.EDU <mehoff@unc.edu>
2024-08-28 09:43:59 -0400

    cq00_functions

---
+++ CQS/cq00_functions.py
@@ -0,0 +1,16 @@
+"""Functions Challenge Question!"""
+
+__author__ = "730684336"
+
+
+def mimic(message: str) -> str:
+    """return message back"""
+    return message
+
+
+def main() -> None:
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lessons/functions.py
@@ -0,0 +1,12 @@
+"""Practice with Functions"""
+
+
+# Define a function
+def sum(num1: int, num2: int) -> int:
+    """Return num 1 + num 2."""
+    return num1 + num2
+
+
+# Call the function
+
+print(sum(num1=1, num2=10))  # <- 1 and 10 are arguments
